2272d57f2a07bfb5478155ab78226bf92f290d25
takuya-jazeera <takuya-jazeera@mineo.jp>
2024-04-14 14:15:31 +0900

    add caption scroll

---
+++ Scripts/TextScroll.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+var t = 0.0
+var qn = Quaternion(0.0,1.0,0.0,0.0)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	qn = quaternion * qn * quaternion.inverse()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	t += delta 
+	position = Vector3(0.0,0.0  ,-2.5) + t * 0.1 * qn.get_axis()
+	#pass
