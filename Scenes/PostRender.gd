1d15867ef863e838f54e1f586e9a555a0c36201f
takuya-jazeera <takuya-jazeera@mineo.jp>
2024-04-14 13:51:44 +0900

    be responsive with respect to the window size

--- Scenes/PostRender.gd
+++ Scenes/PostRender.gd
@@ -1,18 +1,34 @@
 extends Node2D
 
 @onready var panel = $Panel
+@onready var view_height = get_window().size.y * 0.6
+@onready var margin_horizontal = get_window().size.x * 0.1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	panel.size = get_window().size
-	panel.get_node("L").size = Vector2i(300,300)
-	panel.get_node("L").position = Vector2i(100,100)
-	panel.get_node("R").size = Vector2i(300,300)
-	panel.get_node("R").position = Vector2i(420,100)
+	pass 
+
 	
-	panel.get_node("L").material.set_shader_parameter("tex",$Main/ViewL.get_texture())
-	panel.get_node("R").material.set_shader_parameter("tex",$Main/ViewR.get_texture())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_draw():
+	panel.size = get_window().size
+	view_height = get_window().size.y * 0.6
+	margin_horizontal = get_window().size.x * 0.1
+	
+	var panel_l = panel.get_node("L")
+	var panel_r = panel.get_node("R")
+	
+	panel.size = get_window().size
+	panel_l.size = Vector2i(view_height,view_height)
+	panel_l.position = Vector2i(margin_horizontal,100)
+	panel_r.size = Vector2i(view_height,view_height)
+	panel_r.position = Vector2i(get_window().size.x- view_height - margin_horizontal,100)
+	
+	panel_l.material.set_shader_parameter("tex",$Main/ViewL.get_texture())
+	panel_r.material.set_shader_parameter("tex",$Main/ViewR.get_texture())
+
